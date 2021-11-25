9f95c771e9764a790e704b686e337341a084b3b6
Kirill Kotov <k.kotov@dev.local>
2021-11-25 15:34:36 +0300

    Добавлены опциональные аргументы N и M

--- BMP/main.cpp
+++ BMP/main.cpp
@@ -4,21 +4,31 @@
 
 
 
-static double n = 80.0/40;
-static double m = 80.0/40;
+static double n = 270.0/540;
+static double m = 270.0/540;
 
 static void help(const char *);
+static void load_nm(const char*, const char*, bool&);
 
 
 
 int
 main(const int argc, const char **argv)
 {
-        if (argc != 2)
-        {
+        bool success = true;
+        switch (argc) {
+        case 4:
+          load_nm(argv[2], argv[3], success);
+        case 2:
+          break;
+
+        default:
             help(*argv);
             return -1;
         }
+      
+        if (!success) 
+          return -1;
 
         std::unique_ptr<Image> image = std::make_unique<Image>(argv[1]);
 
@@ -41,5 +51,19 @@ main(const int argc, const char **argv)
 static void
 help(const char *prog)
 {
-        std::cout << "[*] Usage: " << std::string(prog) << " image.bmp" << std::endl;
+        std::cout << "[*] Usage: " << std::string(prog) << " image.bmp [N] [M]" << std::endl;
+}
+
+static void
+load_nm(const char *n_, const char *m_, bool &success)
+{
+        try {
+            n = std::stod(n_);
+            m = std::stod(m_);
+            if (n <= 0 || m <= 0)
+              throw std::runtime_error("N and M should be positive and not equal zero.");
+        } catch (std::exception &e) {
+            std::cerr << "Invalid value of N and/or M; " << e.what() << std::endl;
+            success = false;
+        }
 }
